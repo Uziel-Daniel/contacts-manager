fcab44f77bf2454c6b344bf093d91ccf78309aa9
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-04-22 18:01:57 -0400

    creating new contacts objects with unique number ID's

--- src/contactsmanager/ContactsList.java
+++ src/contactsmanager/ContactsList.java
@@ -1,5 +1,6 @@
 package contactsmanager;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class ContactsList {
@@ -7,9 +8,8 @@ public class ContactsList {
     /* CREATE NEW HASHMAP */
     static HashMap<Integer, ContactObject> contactsInfo = new HashMap<>();
 
+    static int counter = 0;
     public static void putContacts() {
-
-        int counter = 0;
 
         ContactObject contact = new ContactObject();
 
@@ -17,8 +17,18 @@ public class ContactsList {
         contact.setPhoneNumber(Input.getUserName("Please enter a contact phone number: "));
 
         /* PUT AND GET FROM HASHMAP */
+        System.out.println(contact.getName() + contact.getPhoneNumber() + " object");
         contactsInfo.put(counter, contact);
-        System.out.println(contactsInfo);
+        for (int key: contactsInfo.keySet()){
+            System.out.print(key + "    |    ");
+//            System.out.println(contactsInfo.get(key));
+
+            System.out.println(contactsInfo.get(key).getName());
+
+            System.out.println(contactsInfo.get(key).getPhoneNumber());
+
+        }
+
 
         counter += 1;
     }
