cab909a514bf4bb59af06b44b82fc570c1605afe
Daniel Zavala <daniel.omar.zavala@gmail.com>
2022-04-22 14:26:40 -0500

    updated src/contactsmanager/ContactsList.java, ContactsManagerApp.java, Input.java incorporated dynamic name and number functionality

--- src/contactsmanager/ContactsList.java
+++ src/contactsmanager/ContactsList.java
@@ -4,26 +4,14 @@ import java.util.HashMap;
 
 public class ContactsList {
 
-    public static void putContacts(String contactName, String phoneNumber) {
+    public static void putContacts() {
 
+        /* CREATE NEW HASHMAP */
         HashMap<String, String> contactsInfo = new HashMap<>();
-        contactsInfo.put(contactName, phoneNumber);
-        ContactsList contactsList = new ContactsList();
-        contactsList.putContacts("tets", "123313133");
 
-        System.out.println(contactsList);
+        /* PUT AND GET FROM HASHMAP */
+        contactsInfo.put(Input.getUserName("Please enter contact name: "), Input.getUserPhoneNumber("Please enter contact phone number: "));
+        System.out.println(contactsInfo);
+
     }
-
-//     public ContactsList(){};
-//
-//
-//    public HashMap<String, String> getContactsInfo() {
-//        return contactsInfo;
-//    }
-//
-//    public void setContactsInfo(String nameContact, String phoneNumber) {
-//        contactsInfo.put(nameContact, phoneNumber);
-//        this.contactsInfo = contactsInfo;
-//    }
-
 }
--- src/contactsmanager/ContactsManagerApp.java
+++ src/contactsmanager/ContactsManagerApp.java
@@ -5,12 +5,9 @@ import java.util.HashMap;
 public class ContactsManagerApp {
 
     public static void main(String[] args) {
-//
-//        System.out.println("It WORKS!...");
-//        ContactsList contactsList = new ContactsList();
 
-      ContactsList.putContacts("test", "2323233");
+      ContactsList.putContacts();
+
+
     }
-
-
 }
